f5c3c0b8dfabb4a71c372ca838a40df374e354e3
yubc <yubc2006@sohu.com>
2018-08-13 22:22:33 +0800

    normal

--- kafka/get-docker-images.sh
+++ kafka/get-docker-images.sh
@@ -4,12 +4,14 @@
 DOCKER_NS=hyperledger
 ARCH=x86_64
 VERSION=1.1.1
-BASE_DOCKER_TAG=X86_64-0.4.6
+BASE_DOCKER_TAG=x86_64-0.4.6
 
-FABRIC_IMAGES=(fabric-peer fabric-orderer fabric-ccenv fabric-javaenv fabric-kafka fabric-zookeeper \
-fabric-couchdb fabric-tools)
+FABRIC_IMAGES=(fabric-peer fabric-orderer fabric-ccenv fabric-javaenv  \
+fabric-tools)
 
 FABRIC_BASE=(fabric-baseos fabric-baseimage)
+
+FABRIC_KAFKA=(fabric-kafka fabric-zookeeper fabric-couchdb)
 
 for image in ${FABRIC_IMAGES[@]}
 do
@@ -17,6 +19,15 @@ do
   docker pull ${DOCKER_NS}/$image:${ARCH}-${VERSION}
   docker tag ${DOCKER_NS}/$image:${ARCH}-${VERSION}  ${DOCKER_NS}/$image:latest
 done
+
+VERSION=1.0.6
+for image in ${FABRIC_KAFKA[@]}
+do
+  echo "Pulling ${DOCKER_NS}/$image:${ARCH}-${VERSION}"
+  docker pull ${DOCKER_NS}/$image:${ARCH}-${VERSION}
+  docker tag ${DOCKER_NS}/$image:${ARCH}-${VERSION}  ${DOCKER_NS}/$image:latest
+done
+
 for base in ${FABRIC_BASE[@]}
 do
   echo "Pulling ${DOCKER_NS}/$base:${BASE_DOCKER_TAG}"
